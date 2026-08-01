931b1970180f78b2981164b2a2c14911f14d62cf
sagar choudhary <sagarchoudhary98164@gmail.com>
2026-08-01 22:13:01 +0530

    db connection

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js"; 
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+
+connectDB()
+
+dotenv.config();
+
+
+// import express from "express"
+// const app = express();
+
+
+// (async()=>{
+//     try{
+//       await  mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//       app.on ("error",(error)=>{
+//         console.log("ERROR :",error);
+//         throw error 
+//       })
+
+//       app.listen(process.env.PORT,()=>{
+//         console.log(`app is running on PORT ${process.env.PORT}`);
+//       })
+
+//     }catch (error){
+//             console.log("ERROR :",error)
+//     }
+    
+// })()
