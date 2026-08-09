de925597cddd50ea5c8506a78aa7b6b27996ee0e
sagar choudhary <sagarchoudhary98164@gmail.com>
2026-08-09 13:42:51 +0530

    add access atoken and refresh token in user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose, {Schema} from "mongoose";
-
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
   
 const userSchema = new Schema(
     {
@@ -19,7 +20,7 @@ const userSchema = new Schema(
             trim: true,
             
         },
-        fullname:{
+        fullName:{
             type: String,
             required : true,
             trim: true,
@@ -53,6 +54,41 @@ const userSchema = new Schema(
     }
 )
 
+userSchema.pre("save", async function(next) {
+    if(!this.isModified("password")) return next();
 
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+userSchema.methods.isPasswordCorrect = async function name
+(password) {
+   return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+   return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreahToken = function(){
+     return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 export const User = mongoose.model("User",userSchema)
