c3eb712f265455f27c5369404afe1d41e6b53319
sagar choudhary <sagarchoudhary98164@gmail.com>
2026-08-27 11:37:05 +0530

    add getUserChannelProfile function and reintroduce subscription model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -342,6 +342,70 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, user, "cover Image updates successfully"));
 });
 
+
+const getUserChannelProfile = asyncHandler(async(req, res)=>{
+
+  const {username} = req.params
+  if (!username?.trim()) {
+      throw new ApiError (400, "user name is missing")
+  }
+
+  const channel = await User.aggregate([
+    {
+      $match: {
+        username: username?.toLowerCase()
+      }
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers"
+      }
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo"
+      }
+    },
+    {
+      $addFields: {
+        subscribersCount: {
+          $size: "subscribers"
+        },
+        channelsSubscribedToCount: {
+          $size: "subscribedTo"
+        },
+        isSubscribed:{
+          $cond: {
+            if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+            then: true,
+            else: false
+          }
+        }
+      }
+    },
+    {
+      $project: {
+        fullName: 1,
+        username: 1,
+        subscribersCount: 1,
+        channelsSubscribedToCount: 1,
+        isSubscribed: 1,
+        avatar: 1,
+        coverImage: 1,
+        email: 1
+
+      }
+    }
+  ])
+  
+})
+
 export {
   registerUser,
   loginUser,
@@ -352,4 +416,5 @@ export {
   updateAccountDetail,
   updateUserAvatar,
   updateUserCoverImage,
+  getUserChannelProfile
 };
